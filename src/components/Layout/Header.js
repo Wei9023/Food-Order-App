d3eb75f4e0b7664e8416f618b596e98d889e0859
Mac <mac@Macs-MBP.router.taotronics.com>
2022-06-23 23:43:02 -0700

    upload

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react/cjs/react.production.min";
+
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from '../../assets/meals.jpeg';
+import classes from './Header.module.css'
+
+const Header = props => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>ReactMeals</h1>
+            <HeaderCartButton onClick={props.onShowCart}/>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="A table fuul of delicious food!" />
+        </div>
+    </Fragment>
+}
+
+export default Header;
